2aef7f7ccee8a660b8fb44c3ec4901369487f7c6
OYDVAT Team <bozoingbozo@gmail.com>
2026-02-08 11:27:35 +0100

    Update vga.h

--- src/vga.h
+++ src/vga.h
@@ -1,4 +1,3 @@
-// vga.h
 #ifndef VGA_H
 #define VGA_H
 
@@ -6,22 +5,32 @@
 #define SCREEN_HEIGHT 200
 #define VRAM_ADDRESS  0xA0000
 
-// Default VGA 256-color palette indices
-#define COLOR_BLACK  0
-#define COLOR_BLUE   1
-#define COLOR_CYAN   3
-#define COLOR_GRAY   7
+// Palette
+#define COLOR_BLACK     0
+#define COLOR_BLUE      1
+#define COLOR_CYAN      3
+#define COLOR_RED       4
+#define COLOR_GRAY      7
 #define COLOR_DARK_GRAY 8
-#define COLOR_WHITE  15
-#define COLOR_RED    4
+#define COLOR_WHITE     15
 
-void put_pixel(int x, int y, unsigned char color);
-void fill_screen(unsigned char color);
-void draw_rect(int x, int y, int w, int h, unsigned char color);
+// Inlined Pixel Functions
+static inline void put_pixel(int x, int y, unsigned char color) {
+    if (x < 0 || x >= SCREEN_WIDTH || y < 0 || y >= SCREEN_HEIGHT) return;
+    unsigned char *vram = (unsigned char *)VRAM_ADDRESS;
+    vram[y * SCREEN_WIDTH + x] = color;
+}
+
 static inline unsigned char get_pixel(int x, int y) {
     if (x < 0 || x >= SCREEN_WIDTH || y < 0 || y >= SCREEN_HEIGHT) return 0;
     unsigned char *vram = (unsigned char *)VRAM_ADDRESS;
     return vram[y * SCREEN_WIDTH + x];
 }
+
+/* --- ADD THESE PROTOTYPES BELOW --- */
+void fill_screen(unsigned char color);
+void draw_rect(int x, int y, int w, int h, unsigned char color);
+void draw_window(int x, int y, int w, int h, const char* title);
+void draw_mouse_cursor(int x, int y);
 
 #endif
