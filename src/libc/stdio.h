3c40168387ad1461833aa79ee570215f059ccac4
OYDVAT Team <bozoingbozo@gmail.com>
2026-02-09 19:41:23 +0100

    Update stdio.h

--- src/libc/stdio.h
+++ src/libc/stdio.h
@@ -2,7 +2,10 @@
 #define LIBC_STDIO_H
 
 #include "stdint.h"
+#include "stdarg.h"
 
 void itoa(int n, char* s, int base);
+int  vsnprintf(char* str, size_t size, const char* format, va_list ap);
+int  sprintf(char* str, const char* format, ...);
 
 #endif
