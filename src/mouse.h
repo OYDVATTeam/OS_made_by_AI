06936325fd0e33c7f81298ca3c42304d5c0d77b5
OYDVAT Team <bozoingbozo@gmail.com>
2026-02-09 20:43:10 +0100

    Update mouse.h

--- src/mouse.h
+++ src/mouse.h
@@ -6,8 +6,12 @@ void mouse_poll(void);
 
 int mouse_get_x(void);
 int mouse_get_y(void);
+int mouse_get_button();
+
+// ADD THIS:
+void mouse_get_state(int *x, int *y, int *button);
 
 void show_mouse(int x, int y);
 void hide_mouse(int x, int y);
-int mouse_get_button();
+
 #endif
