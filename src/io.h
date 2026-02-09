7fb190e750d12e8c5e5e032cb175821ee0d59118
OYDVAT Team <bozoingbozo@gmail.com>
2026-02-09 19:28:12 +0100

    Update io.h

--- src/io.h
+++ src/io.h
@@ -1,13 +1,19 @@
 #ifndef IO_H
 #define IO_H
 
-static inline void outb(unsigned short port, unsigned char value) {
-    asm volatile ("outb %0, %1" : : "a"(value), "Nd"(port));
+static inline void outb(unsigned short port, unsigned char val) {
+    __asm__ volatile ("outb %0, %1" : : "a"(val), "Nd"(port));
 }
 
 static inline unsigned char inb(unsigned short port) {
     unsigned char ret;
-    asm volatile ("inb %1, %0" : "=a"(ret) : "Nd"(port));
+    __asm__ volatile ("inb %1, %0" : "=a"(ret) : "Nd"(port));
+    return ret;
+}
+
+static inline unsigned short inw(unsigned short port) {
+    unsigned short ret;
+    __asm__ volatile ("inw %1, %0" : "=a"(ret) : "Nd"(port));
     return ret;
 }
 
