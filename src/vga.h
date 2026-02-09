56fb04c0622281626f4811ab46db9963abb7cb9b
OYDVAT Team <bozoingbozo@gmail.com>
2026-02-09 20:11:57 +0100

    Update vga.h

--- src/vga.h
+++ src/vga.h
@@ -5,14 +5,22 @@
 #define SCREEN_HEIGHT 200
 #define VRAM_ADDRESS  0xA0000
 
-// Palette
-#define COLOR_BLACK     0
-#define COLOR_BLUE      1
-#define COLOR_CYAN      3
-#define COLOR_RED       4
-#define COLOR_GRAY      7
-#define COLOR_DARK_GRAY 8
-#define COLOR_WHITE     15
+#define COLOR_BLACK   0x00
+#define COLOR_BLUE    0x01
+#define COLOR_GREEN   0x02
+#define COLOR_CYAN    0x03
+#define COLOR_RED     0x04
+#define COLOR_MAGENTA 0x05
+#define COLOR_BROWN   0x06
+#define COLOR_LIGHT_GRAY 0x07
+#define COLOR_GRAY    0x08
+#define COLOR_LIGHT_BLUE 0x09
+#define COLOR_LIGHT_GREEN 0x0A
+#define COLOR_LIGHT_CYAN 0x0B
+#define COLOR_LIGHT_RED 0x0C
+#define COLOR_LIGHT_MAGENTA 0x0D
+#define COLOR_YELLOW  0x0E  // The one that was missing!
+#define COLOR_WHITE   0x0F
 
 // Inlined Pixel Functions
 static inline void put_pixel(int x, int y, unsigned char color) {
