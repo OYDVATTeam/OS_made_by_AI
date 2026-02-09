fce46deedc99aa95bf65c68b58e8ce1fbfc8b62b
OYDVAT Team <bozoingbozo@gmail.com>
2026-02-09 19:38:50 +0100

    Create ctype.c

---
+++ src/libc/ctype.c
@@ -0,0 +1,27 @@
+#include "ctype.h"
+
+int isspace(int c) {
+    return (c == ' ' || c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r');
+}
+
+int isdigit(int c) {
+    return (c >= '0' && c <= '9');
+}
+
+int isupper(int c) {
+    return (c >= 'A' && c <= 'Z');
+}
+
+int islower(int c) {
+    return (c >= 'a' && c <= 'z');
+}
+
+int toupper(int c) {
+    if (islower(c)) return c - 0x20;
+    return c;
+}
+
+int tolower(int c) {
+    if (isupper(c)) return c + 0x20;
+    return c;
+}
