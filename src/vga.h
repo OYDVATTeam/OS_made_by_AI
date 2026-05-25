fdab13a79dc511833ccfa0dcb70e259e36f1204c
OYDVAT Team <bozoingbozo@gmail.com>
2026-05-25 21:17:07 +0200

    add black 4.0 lol

--- src/vga.h
+++ src/vga.h
@@ -6,6 +6,7 @@
 #define VRAM_ADDRESS  0xA0000
 
 #define COLOR_BLACK   0x00
+#define COLOR_BLACK_4_0    0x00 // lol, black 4.0, the blackest black (only on oleds lol)
 #define COLOR_BLUE    0x01
 #define COLOR_GREEN   0x02
 #define COLOR_CYAN    0x03
@@ -20,7 +21,7 @@
 #define COLOR_LIGHT_CYAN 0x0B
 #define COLOR_LIGHT_RED 0x0C
 #define COLOR_LIGHT_MAGENTA 0x0D
-#define COLOR_YELLOW  0x0E  // The one that was missing!
+#define COLOR_YELLOW  0x0E  
 #define COLOR_WHITE   0x0F
 
 extern unsigned char* vram;
@@ -37,7 +38,6 @@ static inline unsigned char get_pixel(int x, int y) {
     return vram[y * SCREEN_WIDTH + x];
 }
 
-/* --- PROTOTYPES --- */
 void vga_init(); // Added: Needed by kernel.c
 void fill_screen(unsigned char color);
 void fill_rect(int x, int y, int w, int h, unsigned char color);     // Added: Was missing!
